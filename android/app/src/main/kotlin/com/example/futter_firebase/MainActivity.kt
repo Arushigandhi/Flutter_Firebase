4c121fdccf8608277db3235d3cff66bef69e7e06
Arushigandhi <75487137+Arushigandhi@users.noreply.github.com>
2021-10-08 22:34:53 +0530

    First commit

---
+++ android/app/src/main/kotlin/com/example/futter_firebase/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.futter_firebase
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Firebase',
+      home: AddData(),
+    );
+  }
+}
+
+class AddData extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: Text("Trying Out Flutter-Fire"),
+      ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('data').snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          return ListView(
+            children: snapshot.data!.docs.map((document) {
+              return Container(
+                child: Center(child: Text(document['text'])),
+              );
+            }).toList(),
+          );
+        },
+      ),
+    );
+  }
+}
